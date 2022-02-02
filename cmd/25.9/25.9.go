64221cb6f70a4dbc1ec9d4ef169479938954ee56
chris <mchead1@naver.com>
2022-02-02 16:45:43 +0900

    :sparkles: Pub / Sub pattern with channels

--- cmd/25.9/25.9.go
+++ cmd/25.9/25.9.go
@@ -1,35 +1,47 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
 
-type Publisher struct {
-	ctx         context.Context
-	subscribeCh chan chan<- string // 일방향 채널
-	publishCh   chan string
-	subsribers  []chan<- string
-}
-
-func NewPublisher(ctx context.Context) *Publisher {
-	return &Publisher{
-		ctx:         ctx,
-		subscribeCh: make(chan chan<- string),
-		publishCh:   make(chan string),
-		subsribers:  make([]chan<- string, 0),
-	}
-}
-
-func (p *Publisher) Publish(msg string) {
-	p.publishCh <- msg
-}
-
-func (p *Publisher) Update() {
-
-}
-
-func (p *Publisher) Subscribe() {
-
-}
+var wg sync.WaitGroup
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
 
+	wg.Add(4)
+
+	publisher := NewPublisher(ctx)
+
+	go publisher.Update()
+
+	subscriber := NewSubscriber("Chris", ctx)
+	subscriber2 := NewSubscriber("Susan", ctx)
+
+	subscriber.Subscribe(publisher)
+	subscriber2.Subscribe(publisher)
+
+	go subscriber.Update()
+	go subscriber2.Update()
+
+	go func() {
+		tick := time.Tick(time.Second * 2)
+		for {
+			select {
+			case <-tick:
+				publisher.Publish("Hello Message")
+			case <-ctx.Done():
+				wg.Done()
+				return
+			}
+		}
+	}()
+
+	fmt.Scanln()
+	cancel()
+
+	wg.Wait()
 }
