8f7dfc532336f9418b28a3168bf2fd9e4731c873
chris <sattlub123@gmail.com>
2022-02-19 15:51:13 +0900

    :sparkles: channel study

--- cmd/25.2/25.2.go
+++ cmd/25.2/25.2.go
@@ -6,16 +6,18 @@ import (
 )
 
 func main() {
-	ch := make(chan int, 2)
+	ch := make(chan int)
 
-	go square()
+	go square(ch)
 	ch <- 9
 	fmt.Println("Never print")
 }
 
-func square() {
+func square(ch chan int) {
 	for {
 		time.Sleep(2 * time.Second)
 		fmt.Println("sleep")
+
+		fmt.Println("value is", <-ch)
 	}
 }
--- cmd/25.7/25.7.go
+++ cmd/25.7/25.7.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"sync"
 	"time"
@@ -15,59 +14,55 @@ type Car struct {
 
 var wg sync.WaitGroup
 var startTime = time.Now()
+var limit = 10
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	tireCh := make(chan *Car)
-	paintCh := make(chan *Car)
-
-	fmt.Println("Start Factory\n")
-
 	wg.Add(3)
+	tireCh := make(chan *Car, limit)
+	paintCh := make(chan *Car, limit)
 
-	go InitBody(ctx, tireCh)
-	go InstallTire(tireCh, paintCh)
-	go PaintCar(paintCh)
-
-	time.Sleep(10 * time.Second)
-	cancel()
+	go makeBody(tireCh, time.Duration(limit), &wg)
+	go installTire(tireCh, paintCh, &wg)
+	go paintCar(paintCh, &wg)
 
 	wg.Wait()
-	fmt.Println("All Done")
 }
 
-func InitBody(ctx context.Context, ch chan *Car) {
+func makeBody(tireCh chan *Car, endSeconds time.Duration, wg *sync.WaitGroup) {
 	tick := time.Tick(time.Second)
+	terminate := time.After(endSeconds * time.Second)
 
 	for {
 		select {
 		case <-tick:
-			ch <- &Car{Body: "Tesla Model 3"}
-		case <-ctx.Done():
-			close(ch)
+			car := &Car{Body: "Make the body"}
+			tireCh <- car
+		case <-terminate:
+			fmt.Println("Work done")
+			close(tireCh)
 			wg.Done()
 			return
 		}
 	}
 }
 
-func InstallTire(tireCh, paintCh chan *Car) {
-	for car := range tireCh {
+func installTire(tireCh chan *Car, paintCh chan *Car, wg *sync.WaitGroup) {
+	for c := range tireCh {
 		time.Sleep(time.Second)
-
-		car.Tire = "Snow tire"
-		paintCh <- car
+		c.Tire = "Snow Tire"
+		paintCh <- c
 	}
 
 	close(paintCh)
 	wg.Done()
 }
 
-func PaintCar(ch chan *Car) {
-	for car := range ch {
-		car.Color = "Yellow"
-		duration := time.Now().Sub(startTime)
-		fmt.Printf("%.2f Complete Car %s %s %s\n", duration.Seconds(), car.Body, car.Tire, car.Color)
+func paintCar(paintCh chan *Car, wg *sync.WaitGroup) {
+	for c := range paintCh {
+		time.Sleep(time.Second)
+		c.Color = "Blue"
+		fmt.Println("The car aws made", c)
 	}
+
 	wg.Done()
 }
