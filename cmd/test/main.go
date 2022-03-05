ed76a1c15329e263432fd7400bcec3e8d0821cb3
chris <sattlub123@gmail.com>
2022-03-06 01:26:58 +0900

    :white_mark_test: fibonacci case's benchmark

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -6,6 +6,35 @@ func square(x int) int {
 	return x * x
 }
 
+func fibonacci(n int) int {
+	if n < 0 {
+		return 0
+	} else if n < 2 {
+		return n
+	}
+
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+
+func fibonacci2(n int) int {
+	if n < 0 {
+		return 0
+	}
+	if n < 2 {
+		return n
+	}
+
+	one := 1
+	two := 0
+	rst := 0
+	for i := 2; i <= n; i++ {
+		rst = one + two
+		two = one
+		one = rst
+	}
+	return rst
+}
+
 func main() {
 	num := 9
 	fmt.Printf("%d x %d = %d\n", num, num, square(num))
