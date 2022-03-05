bdba117fae5b07979bde801fcf37f90cb4351fa4
chris <sattlub123@gmail.com>
2022-03-06 01:36:13 +0900

    :white_mark_test: 테스트케이스 예제 작성

--- cmd/test/28_test.go
+++ cmd/test/28_test.go
@@ -9,3 +9,11 @@ func TestSquare2(t *testing.T) {
 	ass := assert.New(t)
 	ass.Equal(9, square(3), "square(3) should be 9")
 }
+
+func TestAtoI(t *testing.T) {
+	ass := assert.New(t)
+	rst, err := AtoI("13")
+
+	ass.Nilf(err, "It was not occurred an error")
+	ass.Equal(13, rst, "string(13) should be int(13)")
+}
