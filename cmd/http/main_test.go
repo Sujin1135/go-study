0edaaa8efbeab46696de86b3af141a5e66c5e2ee
chris <mchead1@naver.com>
2022-03-06 22:21:06 +0900

    :sparkles: json response test

--- cmd/http/main_test.go
+++ cmd/http/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"io"
@@ -37,4 +38,25 @@ func TestIndexPathHandlerWithPostMethod(t *testing.T) {
 	assert.Equal(http.StatusOK, res.Code)
 	data, _ := io.ReadAll(res.Body)
 	assert.Equal(fmt.Sprintf("Requested method is %s\n", method), string(data))
+}
+
+func TestStudentPostHandler(t *testing.T) {
+	assert := assert.New(t)
+	method := "POST"
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest(method, "/students", nil)
+
+	mux := NewWebHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusCreated, res.Code)
+	body := new(student)
+	err := json.NewDecoder(res.Body).Decode(body)
+	assert.Nil(err)
+	expected := newStudent("철수", 59, 100)
+
+	assert.Equal(body.Name, expected.Name)
+	assert.Equal(body.Age, expected.Age)
+	assert.Equal(body.Score, expected.Score)
 }
