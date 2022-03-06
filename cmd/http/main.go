d74e71c5ac22c7f7b5898a2482fad896203d8d36
chris <mchead1@naver.com>
2022-03-06 15:00:21 +0900

    :tada: listen 3000 port

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func IndexPathHandler(w http.ResponseWriter, r *http.Request) {
+	//	TODO: write the code
+	fmt.Fprint(w, "Hello World")
+}
+
+func main() {
+	http.HandleFunc("/", IndexPathHandler)
+	http.ListenAndServe(":3000", nil)
+}
